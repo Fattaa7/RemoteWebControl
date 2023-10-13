c610bea1548ada3d5b3ba47f86e7cf63a49ca1f2
Fattaa7 <ahmed.qweq@hotmail.com>
2023-10-13 23:13:36 +0300

    added separate driver for keypad interaction

--- keypadcontrol.py
+++ keypadcontrol.py
@@ -6,66 +6,10 @@ import drivers
 import threading
 from time import sleep, strftime
 import audioFuncs
+import padkeydriver
 
-
-# Define the GPIO pins for the keypad rows and columns
-rows = [19, 26, 16, 20, 12]  # GPIO 19, 26, 16, 20
-cols = [21, 13, 6, 5]  # GPIO 21, 13, 6, 5, 12
-cursor = 0
-input_text = ""
-
-# Define the keypad matrix
-
-keys = [
-    ['F1', 'F2', '=', '*'],
-    ['1', '2', '3', 'Up'],
-    ['4', '5', '6', 'Down'],
-    ['7', '8', '9', 'Esc'],
-    ['-', '0', '+', 'Enter']
-]
-
-
-
-# Setup GPIO mode and initial state
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-
-
-# Setup row pins as outputs and set them to high
-for row in rows:
-    GPIO.setup(row, GPIO.OUT)
-    GPIO.output(row, GPIO.HIGH)
-
-
-
-# Setup column pins as inputs with pull-up resistors
-for col in cols:
-    GPIO.setup(col, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
-
-# Function to read the keypad
-
-def read_keypad():
-    key = None
-    # Scan rows
-    for i, row in enumerate(rows):
-        GPIO.output(row, GPIO.LOW)
-        # Check column inputs
-        for j, col in enumerate(cols):
-            if GPIO.input(col) == GPIO.LOW:
-                key = keys[i][j]
-                time.sleep(0.1)  # Debounce delay
-        GPIO.output(row, GPIO.HIGH)
-    return key
-
-audioFuncs.audio_thread_function()
-
-audioFuncs.autoNext()
-
-while True:
-    
-    pressed_key = read_keypad()
+def mainLoop():
+    pressed_key = padkeydriver.read_keypad()
     if pressed_key is not None:
         print("Pressed key:", pressed_key)
         if pressed_key == '9':
@@ -78,6 +22,17 @@ while True:
             audioFuncs.forward()
         if pressed_key == '=':
             audioFuncs.backward()
+    threading.Timer(0.3, mainLoop).start()
+
     
+
+
+padkeydriver.init()
+audioFuncs.audio_thread_function()
+audioFuncs.autoNext()
+mainLoop()
+
+
+while True:
+    break    
     
-    time.sleep(0.3)  # Delay between scans
---
+++ padkeydriver.py
@@ -0,0 +1,61 @@
+import boto3
+import vlc
+import RPi.GPIO as GPIO
+import time
+import drivers
+import threading
+from time import sleep, strftime
+import audioFuncs
+
+# Define the GPIO pins for the keypad rows and columns
+rows = [19, 26, 16, 20, 12]  # GPIO 19, 26, 16, 20
+cols = [21, 13, 6, 5]  # GPIO 21, 13, 6, 5, 12
+cursor = 0
+input_text = ""
+# Define the keypad matrix
+keys = [
+    ['F1', 'F2', '=', '*'],
+    ['1', '2', '3', 'Up'],
+    ['4', '5', '6', 'Down'],
+    ['7', '8', '9', 'Esc'],
+    ['-', '0', '+', 'Enter']
+]
+
+
+
+def init():
+
+
+    # Setup GPIO mode and initial state
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+
+    # Setup row pins as outputs and set them to high
+    for row in rows:
+        GPIO.setup(row, GPIO.OUT)
+        GPIO.output(row, GPIO.HIGH)
+
+
+
+    # Setup column pins as inputs with pull-up resistors
+    for col in cols:
+        GPIO.setup(col, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+
+
+    # Function to read the keypad
+
+def read_keypad():
+    key = None
+        # Scan rows
+    for i, row in enumerate(rows):
+        GPIO.output(row, GPIO.LOW)
+        # Check column inputs
+        for j, col in enumerate(cols):
+            if GPIO.input(col) == GPIO.LOW:
+                key = keys[i][j]
+                time.sleep(0.1)  # Debounce delay
+        GPIO.output(row, GPIO.HIGH)
+    return key
+
